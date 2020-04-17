7a5c03c29624309811bbc9b12f8890654d7a29ac
Isaac Kiyimba <isaaceduardk@gmail.com>
2020-04-17 14:14:55 +0300

    Doggle Visibility demo

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,126 +1,41 @@
 "use strict";
 
-var user = {
-    name: "Jimmy Bullard",
-    age: 18,
-    location: 'London',
-    options: ["One", "Two"],
-    displayOptions: function displayOptions() {
-        this.options.forEach(function (item) {
-            console.log(item + ' is also an option');
-        });
-    },
-    displayWithMap: function displayWithMap() {
-        return this.options.map(function (item) {
-            return item + " option from map";
-        });
-    }
-};
-
-var template = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        user.name ? user.name : 'Anonymous'
-    ),
-    user.age && React.createElement(
-        "p",
-        null,
-        "Age : ",
-        user.age
-    ),
-    user.age < 18 ? React.createElement(
-        "p",
-        null,
-        "Age Group : Minor"
-    ) : React.createElement(
-        "p",
-        null,
-        "Age Group : Senior"
-    ),
-    getLocation(user.location),
-    React.createElement(
-        "p",
-        null,
-        user.options.length > 0 ? "Here are your options" : "No Options"
-    ),
-    React.createElement(
-        "ol",
-        null,
-        React.createElement(
-            "li",
-            null,
-            "Item 1"
-        ),
-        React.createElement(
-            "li",
-            null,
-            "Item 2"
-        )
-    )
-);
-
-function getLocation(location) {
-    if (location) {
-        return React.createElement(
-            "p",
-            null,
-            "Location: ",
-            location
-        );
-    }
-}
-
-var count = 0;
-
-var addOne = function addOne() {
-    count = count + 1;
-    renderApp();
-};
-
-var minusOne = function minusOne() {
-    count = count - 1;
-    renderApp();
-};
-
-var reset = function reset() {
-    count = 0;
-    renderApp();
-};
-
 var appRoot = document.getElementById("app");
 
-var renderApp = function renderApp() {
+var toggle = false;
 
-    var counterTemplate = React.createElement(
+var onToggleSection = function onToggleSection() {
+    if (toggle) {
+        toggle = false;
+    } else {
+        toggle = true;
+    }
+    render();
+};
+
+var render = function render() {
+
+    var template = React.createElement(
         "div",
         null,
         React.createElement(
             "h1",
             null,
-            "Count: ",
-            count
+            "Visibility Toggle"
         ),
         React.createElement(
             "button",
-            { onClick: addOne },
-            "+1"
+            { onClick: onToggleSection },
+            toggle ? "Hide details" : "Show details"
         ),
         React.createElement(
-            "button",
-            { onClick: minusOne },
-            "-1"
-        ),
-        React.createElement(
-            "button",
-            { onClick: reset },
-            "Reset"
+            "p",
+            { style: { visibility: toggle ? "hidden" : "visible" } },
+            "Hey. These are some details you can see!"
         )
     );
 
-    ReactDOM.render(counterTemplate, appRoot);
+    ReactDOM.render(template, appRoot);
 };
 
-renderApp();
+render();
--- src/app.js
+++ src/app.js
@@ -1,74 +1,27 @@
-
-const user = {
-    name : "Jimmy Bullard",
-    age : 18,
-    location : 'London',
-    options : [ "One", "Two"],
-    displayOptions : function(){
-        this.options.forEach((item) => { console.log(item + ' is also an option')})
-    },
-    displayWithMap(){
-        return this.options.map((item) => {
-            return item + " option from map";
-        })
-    }
-
-}
-
-const template = (
-    <div>
-    <h1>{user.name ? user.name : 'Anonymous'}</h1>
-    {user.age && <p>Age : {user.age}</p>}
-    {user.age < 18 ? <p>Age Group : Minor</p> : <p>Age Group : Senior</p>}
-    {getLocation(user.location)}
-    <p>{user.options.length > 0 ? "Here are your options" : "No Options"}</p>
-    <ol>
-        <li>Item 1</li>
-        <li>Item 2</li>
-    </ol>
-    </div>
-);
-
-
-function getLocation(location){
-    if(location){
-        return <p>Location: {location}</p>
-    }
-}
-
-let count = 0;
-
-const addOne = () => {
-    count = count+1;
-    renderApp();
-};
-
-const minusOne = () => {
-    count = count-1;
-    renderApp();
-}
-
-const reset = () => {
-    count = 0;
-    renderApp();
-}
-
-
 const appRoot = document.getElementById("app");
 
-const renderApp = () => {
+let toggle = false;
 
-    const counterTemplate = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>Reset</button>
-        </div>
-    );
-
-    ReactDOM.render(counterTemplate,appRoot);
+const onToggleSection = () => {
+    if(toggle){
+        toggle = false;
+    }else{
+        toggle = true;
+    }
+    render()
 }
 
-renderApp();
+const render = () => {
 
+    const template = (
+        <div>
+            <h1>Visibility Toggle</h1>
+            <button onClick={onToggleSection}>{toggle ? "Hide details" : "Show details"}</button>
+            <p style={{visibility : toggle ? "hidden" : "visible"}}>Hey. These are some details you can see!</p>
+        </div>
+    );
+    
+    ReactDOM.render(template,appRoot)
+}
+
+render();
