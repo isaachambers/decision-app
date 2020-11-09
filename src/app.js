408c66b0ed0a1d92d98fa221143c40aece29ba57
Isaac Kiyimba <isaaceduardk@gmail.com>
2020-11-09 23:42:50 +0300

    Configuring webpack

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import DecisionApp from "./components/DecisionApp"
+import './styles/style.scss'
 
 ReactDOM.render(<DecisionApp/>,
     document.getElementById("app")
-)
+) 
